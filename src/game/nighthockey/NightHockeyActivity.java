156860072ede9255ed272711ce63443f64c06252
Janne Välimaa <janne.valimaa@gmail.com>
2012-10-04 20:36:48 +0300

    Connects client to server

--- src/game/nighthockey/NightHockeyActivity.java
+++ src/game/nighthockey/NightHockeyActivity.java
@@ -1,5 +1,10 @@
 package game.nighthockey;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -8,27 +13,58 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.IOnAreaTouchListener;
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.ITouchArea;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.multiplayer.protocol.adt.message.IMessage;
+import org.andengine.extension.multiplayer.protocol.adt.message.server.IServerMessage;
+import org.andengine.extension.multiplayer.protocol.adt.message.server.ServerMessage;
+import org.andengine.extension.multiplayer.protocol.client.IServerMessageHandler;
+import org.andengine.extension.multiplayer.protocol.client.connector.ServerConnector;
+import org.andengine.extension.multiplayer.protocol.client.connector.SocketConnectionServerConnector;
+import org.andengine.extension.multiplayer.protocol.client.connector.SocketConnectionServerConnector.ISocketConnectionServerConnectorListener;
+import org.andengine.extension.multiplayer.protocol.server.SocketServer;
+import org.andengine.extension.multiplayer.protocol.server.SocketServer.ISocketServerListener;
+import org.andengine.extension.multiplayer.protocol.server.connector.ClientConnector;
+import org.andengine.extension.multiplayer.protocol.server.connector.SocketConnectionClientConnector;
+import org.andengine.extension.multiplayer.protocol.server.connector.SocketConnectionClientConnector.ISocketConnectionClientConnectorListener;
+import org.andengine.extension.multiplayer.protocol.shared.SocketConnection;
+import org.andengine.extension.multiplayer.protocol.util.MessagePool;
+import org.andengine.extension.multiplayer.protocol.util.WifiUtils;
 import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.debug.Debug;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-public class NightHockeyActivity extends SimpleBaseGameActivity {
+public class NightHockeyActivity extends SimpleBaseGameActivity implements ClientMessageFlags, ServerMessageFlags  {
 	/* Const varibles */
 	private static final int CAMERA_WIDTH = 800;
 	private static final int CAMERA_HEIGHT = 480;
@@ -46,6 +82,10 @@ public class NightHockeyActivity extends SimpleBaseGameActivity {
 	
 	/* HockeyPlayers */
 	ArrayList<HockeyPlayer> hockeyPlayers = new ArrayList<HockeyPlayer>();	
+	
+	public NightHockeyActivity() {
+		this.initMessagePool();
+	}
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -112,7 +152,9 @@ public class NightHockeyActivity extends SimpleBaseGameActivity {
 		scene.attachChild(right);
 		
 		resetHockeyPlayers();
-
+		
+		onCreateScene2(scene);
+		
 		return scene;
 	}
 
@@ -151,5 +193,447 @@ public class NightHockeyActivity extends SimpleBaseGameActivity {
 	@Override
 	public void onPauseGame() {
 		super.onPauseGame();
+	}
+	
+	
+	
+	/*************************************************************************************/
+	
+	
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final String LOCALHOST_IP = "127.0.0.1";
+
+	private static final int SERVER_PORT = 4444;
+
+	private static final short FLAG_MESSAGE_SERVER_ADD_FACE = 1;
+	private static final short FLAG_MESSAGE_SERVER_MOVE_FACE = FLAG_MESSAGE_SERVER_ADD_FACE + 1;
+
+	private static final int DIALOG_CHOOSE_SERVER_OR_CLIENT_ID = 0;
+	private static final int DIALOG_ENTER_SERVER_IP_ID = DIALOG_CHOOSE_SERVER_OR_CLIENT_ID + 1;
+	private static final int DIALOG_SHOW_SERVER_IP_ID = DIALOG_ENTER_SERVER_IP_ID + 1;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private BitmapTextureAtlas mBitmapTextureAtlas;
+	private ITextureRegion mFaceTextureRegion;
+
+	private int mFaceIDCounter;
+	private final SparseArray<Sprite> mFaces = new SparseArray<Sprite>();
+
+	private String mServerIP = LOCALHOST_IP;
+	private SocketServer<SocketConnectionClientConnector> mSocketServer;
+	private ServerConnector<SocketConnection> mServerConnector;
+
+	private final MessagePool<IMessage> mMessagePool = new MessagePool<IMessage>();
+
+	private void initMessagePool() {
+		this.mMessagePool.registerMessage(FLAG_MESSAGE_SERVER_ADD_FACE, AddFaceServerMessage.class);
+		this.mMessagePool.registerMessage(FLAG_MESSAGE_SERVER_MOVE_FACE, MoveFaceServerMessage.class);
+	}
+
+
+	public Scene onCreateScene2(Scene scene) {
+		Log.i("NETWORK", "onScene2");
+		/* We allow only the server to actively send around messages. */
+		if(mSocketServer != null) {
+			scene.setOnSceneTouchListener(new IOnSceneTouchListener() {
+				@Override
+				public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+					if(pSceneTouchEvent.isActionDown()) {
+						try {
+							final AddFaceServerMessage addFaceServerMessage = (AddFaceServerMessage) mMessagePool.obtainMessage(FLAG_MESSAGE_SERVER_ADD_FACE);
+							addFaceServerMessage.set(mFaceIDCounter++, pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+
+							mSocketServer.sendBroadcastServerMessage(addFaceServerMessage);
+
+							mMessagePool.recycleMessage(addFaceServerMessage);
+						} catch (final IOException e) {
+							Debug.e(e);
+						}
+						return true;
+					} else {
+						return true;
+					}
+				}
+			});
+
+			scene.setOnAreaTouchListener(new IOnAreaTouchListener() {
+				@Override
+				public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final ITouchArea pTouchArea, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+					try {
+						final Sprite face = (Sprite)pTouchArea;
+						final Integer faceID = (Integer)face.getUserData();
+
+						final MoveFaceServerMessage moveFaceServerMessage = (MoveFaceServerMessage) mMessagePool.obtainMessage(FLAG_MESSAGE_SERVER_MOVE_FACE);
+						moveFaceServerMessage.set(faceID, pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+
+						mSocketServer.sendBroadcastServerMessage(moveFaceServerMessage);
+
+						mMessagePool.recycleMessage(moveFaceServerMessage);
+					} catch (final IOException e) {
+						Debug.e(e);
+						return false;
+					}
+					return true;
+				}
+			});
+
+			scene.setTouchAreaBindingOnActionDownEnabled(true);
+		}
+		
+		initServerAndClient();
+
+		return scene;
+	}
+
+	@Override
+	protected Dialog onCreateDialog(final int pID) {
+		switch(pID) {
+			case DIALOG_SHOW_SERVER_IP_ID:
+				try {
+					return new AlertDialog.Builder(this)
+					.setIcon(android.R.drawable.ic_dialog_info)
+					.setTitle("Your Server-IP ...")
+					.setCancelable(false)
+					.setMessage("The IP of your Server is:\n" + WifiUtils.getWifiIPv4Address(this))
+					.setPositiveButton(android.R.string.ok, null)
+					.create();
+				} catch (final UnknownHostException e) {
+					return new AlertDialog.Builder(this)
+					.setIcon(android.R.drawable.ic_dialog_alert)
+					.setTitle("Your Server-IP ...")
+					.setCancelable(false)
+					.setMessage("Error retrieving IP of your Server: " + e)
+					.setPositiveButton(android.R.string.ok, new OnClickListener() {
+						@Override
+						public void onClick(final DialogInterface pDialog, final int pWhich) {
+							finish();
+						}
+					})
+					.create();
+				}
+			case DIALOG_ENTER_SERVER_IP_ID:
+				final EditText ipEditText = new EditText(this);
+				return new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_info)
+				.setTitle("Enter Server-IP ...")
+				.setCancelable(false)
+				.setView(ipEditText)
+				.setPositiveButton("Connect", new OnClickListener() {
+					@Override
+					public void onClick(final DialogInterface pDialog, final int pWhich) {
+						mServerIP = ipEditText.getText().toString();
+						initClient();
+					}
+				})
+				.setNegativeButton(android.R.string.cancel, new OnClickListener() {
+					@Override
+					public void onClick(final DialogInterface pDialog, final int pWhich) {
+						finish();
+					}
+				})
+				.create();
+			case DIALOG_CHOOSE_SERVER_OR_CLIENT_ID:
+				return new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_info)
+				.setTitle("Be Server or Client ...")
+				.setCancelable(false)
+				.setPositiveButton("Client", new OnClickListener() {
+					@Override
+					public void onClick(final DialogInterface pDialog, final int pWhich) {
+						showDialog(DIALOG_ENTER_SERVER_IP_ID);
+					}
+				})
+				.setNeutralButton("Server", new OnClickListener() {
+					@Override
+					public void onClick(final DialogInterface pDialog, final int pWhich) {
+						toast("You can add and move sprites, which are only shown on the clients.");
+						initServer();
+						showDialog(DIALOG_SHOW_SERVER_IP_ID);
+					}
+				})
+				.setNegativeButton("Both", new OnClickListener() {
+					@Override
+					public void onClick(final DialogInterface pDialog, final int pWhich) {
+						toast("You can add sprites and move them, by dragging them.");
+						initServerAndClient();
+						showDialog(DIALOG_SHOW_SERVER_IP_ID);
+					}
+				})
+				.create();
+			default:
+				return super.onCreateDialog(pID);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		if(this.mSocketServer != null) {
+			try {
+				mSocketServer.sendBroadcastServerMessage(new ConnectionCloseServerMessage());
+			} catch (final IOException e) {
+				Debug.e(e);
+			}
+			mSocketServer.terminate();
+		}
+
+		if(mServerConnector != null) {
+			mServerConnector.terminate();
+		}
+
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyUp(final int pKeyCode, final KeyEvent pEvent) {
+		switch(pKeyCode) {
+			case KeyEvent.KEYCODE_BACK:
+				this.finish();
+				return true;
+		}
+		return super.onKeyUp(pKeyCode, pEvent);
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public void addFace(final int pID, final float pX, final float pY) {
+		final Scene scene = this.mEngine.getScene();
+		/* Create the face and add it to the scene. */
+		final Sprite face = new Sprite(0, 0, this.mFaceTextureRegion, this.getVertexBufferObjectManager());
+		face.setPosition(pX - face.getWidth() * 0.5f, pY - face.getHeight() * 0.5f);
+		face.setUserData(pID);
+		this.mFaces.put(pID, face);
+		scene.registerTouchArea(face);
+		scene.attachChild(face);
+	}
+
+	public void moveFace(final int pID, final float pX, final float pY) {
+		/* Find and move the face. */
+		final Sprite face = this.mFaces.get(pID);
+		face.setPosition(pX - face.getWidth() * 0.5f, pY - face.getHeight() * 0.5f);
+	}
+
+	private void initServerAndClient() {
+		Log.i("NETWORK", "initClientAndServer");
+		this.initServer();
+
+		/* Wait some time after the server has been started, so it actually can start up. */
+		try {
+			Thread.sleep(500);
+		} catch (final Throwable t) {
+			Debug.e(t);
+		}
+
+		this.initClient();
+	}
+
+	private void initServer() {
+		Log.i("NETWORK", "initServer");
+		mSocketServer = new SocketServer<SocketConnectionClientConnector>(SERVER_PORT, new ExampleClientConnectorListener(), new ExampleServerStateListener()) {
+			@Override
+			protected SocketConnectionClientConnector newClientConnector(final SocketConnection pSocketConnection) throws IOException {
+				Log.i("NETWORK", "newClientConnector");
+				return new SocketConnectionClientConnector(pSocketConnection);
+			}
+		};
+
+		mSocketServer.start();
+	}
+
+	private void initClient() {
+		Log.i("NETWORK", "initClient");
+		try {
+			mServerConnector = new SocketConnectionServerConnector(new SocketConnection(new Socket(this.mServerIP, SERVER_PORT)), new ExampleServerConnectorListener());
+
+			mServerConnector.registerServerMessage(FLAG_MESSAGE_SERVER_CONNECTION_CLOSE, ConnectionCloseServerMessage.class, new IServerMessageHandler<SocketConnection>() {
+				@Override
+				public void onHandleMessage(final ServerConnector<SocketConnection> pServerConnector, final IServerMessage pServerMessage) throws IOException {
+					Log.i("NETWORK", "onHandleMessage");
+					finish();
+				}
+			});
+
+			mServerConnector.registerServerMessage(FLAG_MESSAGE_SERVER_ADD_FACE, AddFaceServerMessage.class, new IServerMessageHandler<SocketConnection>() {
+				@Override
+				public void onHandleMessage(final ServerConnector<SocketConnection> pServerConnector, final IServerMessage pServerMessage) throws IOException {
+					Log.i("NETWORK", "onHandleMessage");
+					final AddFaceServerMessage addFaceServerMessage = (AddFaceServerMessage)pServerMessage;
+					addFace(addFaceServerMessage.mID, addFaceServerMessage.mX, addFaceServerMessage.mY);
+				}
+			});
+
+			mServerConnector.registerServerMessage(FLAG_MESSAGE_SERVER_MOVE_FACE, MoveFaceServerMessage.class, new IServerMessageHandler<SocketConnection>() {
+				@Override
+				public void onHandleMessage(final ServerConnector<SocketConnection> pServerConnector, final IServerMessage pServerMessage) throws IOException {
+					Log.i("NETWORK", "onHandleMessage");
+					final MoveFaceServerMessage moveFaceServerMessage = (MoveFaceServerMessage)pServerMessage;
+					moveFace(moveFaceServerMessage.mID, moveFaceServerMessage.mX, moveFaceServerMessage.mY);
+				}
+			});
+
+			mServerConnector.getConnection().start();
+		} catch (final Throwable t) {
+			Debug.e(t);
+		}
+	}
+
+	private void log(final String pMessage) {
+		Debug.d(pMessage);
+	}
+
+	private void toast(final String pMessage) {
+		this.log(pMessage);
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(NightHockeyActivity.this, pMessage, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+	public static class AddFaceServerMessage extends ServerMessage {
+		private int mID;
+		private float mX;
+		private float mY;
+
+		public AddFaceServerMessage() {
+
+		}
+
+		public AddFaceServerMessage(final int pID, final float pX, final float pY) {
+			this.mID = pID;
+			this.mX = pX;
+			this.mY = pY;
+		}
+
+		public void set(final int pID, final float pX, final float pY) {
+			this.mID = pID;
+			this.mX = pX;
+			this.mY = pY;
+		}
+
+		@Override
+		public short getFlag() {
+			return FLAG_MESSAGE_SERVER_ADD_FACE;
+		}
+
+		@Override
+		protected void onReadTransmissionData(final DataInputStream pDataInputStream) throws IOException {
+			Log.i("NETWORK", "onReadTransmissionData");
+			this.mID = pDataInputStream.readInt();
+			this.mX = pDataInputStream.readFloat();
+			this.mY = pDataInputStream.readFloat();
+		}
+
+		@Override
+		protected void onWriteTransmissionData(final DataOutputStream pDataOutputStream) throws IOException {
+			Log.i("NETWORK", "onWriteTransmissionData");
+			pDataOutputStream.writeInt(this.mID);
+			pDataOutputStream.writeFloat(this.mX);
+			pDataOutputStream.writeFloat(this.mY);
+		}
+	}
+
+	public static class MoveFaceServerMessage extends ServerMessage {
+		private int mID;
+		private float mX;
+		private float mY;
+
+		public MoveFaceServerMessage() {
+
+		}
+
+		public MoveFaceServerMessage(final int pID, final float pX, final float pY) {
+			this.mID = pID;
+			this.mX = pX;
+			this.mY = pY;
+		}
+
+		public void set(final int pID, final float pX, final float pY) {
+			this.mID = pID;
+			this.mX = pX;
+			this.mY = pY;
+		}
+
+		@Override
+		public short getFlag() {
+			return FLAG_MESSAGE_SERVER_MOVE_FACE;
+		}
+
+		@Override
+		protected void onReadTransmissionData(final DataInputStream pDataInputStream) throws IOException {
+			Log.i("NETWORK", "onReadTransmissionData");
+			this.mID = pDataInputStream.readInt();
+			this.mX = pDataInputStream.readFloat();
+			this.mY = pDataInputStream.readFloat();
+		}
+
+		@Override
+		protected void onWriteTransmissionData(final DataOutputStream pDataOutputStream) throws IOException {
+			Log.i("NETWORK", "onWriteTransmissionData");
+			pDataOutputStream.writeInt(this.mID);
+			pDataOutputStream.writeFloat(this.mX);
+			pDataOutputStream.writeFloat(this.mY);
+		}
+	}
+
+	private class ExampleServerConnectorListener implements ISocketConnectionServerConnectorListener {
+		@Override
+		public void onStarted(final ServerConnector<SocketConnection> pConnector) {
+			toast("CLIENT: Connected to server.");
+			Log.i("NETWORK", "onStarted");
+		}
+
+		@Override
+		public void onTerminated(final ServerConnector<SocketConnection> pConnector) {
+			toast("CLIENT: Disconnected from Server...");
+			Log.i("NETWORK", "onTerminated");
+			finish();
+		}
+	}
+
+	private class ExampleServerStateListener implements ISocketServerListener<SocketConnectionClientConnector> {
+		@Override
+		public void onStarted(final SocketServer<SocketConnectionClientConnector> pSocketServer) {
+			toast("SERVER: Started.");
+			Log.i("NETWORK", "onStarted");
+		}
+
+		@Override
+		public void onTerminated(final SocketServer<SocketConnectionClientConnector> pSocketServer) {
+			toast("SERVER: Terminated.");
+			Log.i("NETWORK", "onTerminated");
+		}
+
+		@Override
+		public void onException(final SocketServer<SocketConnectionClientConnector> pSocketServer, final Throwable pThrowable) {
+			Debug.e(pThrowable);
+			toast("SERVER: Exception: " + pThrowable);
+		}
+	}
+
+	private class ExampleClientConnectorListener implements ISocketConnectionClientConnectorListener {
+		@Override
+		public void onStarted(final ClientConnector<SocketConnection> pConnector) {
+			toast("SERVER: Client connected: " + pConnector.getConnection().getSocket().getInetAddress().getHostAddress());
+			Log.i("NETWORK", "onStarted");
+		}
+
+		@Override
+		public void onTerminated(final ClientConnector<SocketConnection> pConnector) {
+			toast("SERVER: Client disconnected: " + pConnector.getConnection().getSocket().getInetAddress().getHostAddress());
+			Log.i("NETWORK", "onTerminated");
+		}
 	}
 }
